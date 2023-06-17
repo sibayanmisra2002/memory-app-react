bd65022209c93586dbb8b4fc3c1416345e9ab09d
Sibayan Misra <sibayanmisra2002@gmail.com>
2023-06-18 01:45:26 +0530

    4th

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, componse } from 'redux';
+import { legacy_createStore as createStore} from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
 
 import App from './App';
 
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById('root'));
